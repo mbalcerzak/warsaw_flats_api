bf62d57bc74843ec1e8b7c2459d22db23878fb6b
mbalcerzak <malgorzata.a.balcerzak@gmail.com>
2021-05-31 19:10:49 +0200

    added print statements to indicate where we stop

--- api/convert_to_json.py
+++ api/convert_to_json.py
@@ -1,9 +1,7 @@
-from collections import Counter
 import json
 import yaml
 import sqlite3
 import pandas as pd
-import psutil
 
 from get_dataframes import load_df, load_df_avg_prices, load_area_cat_df
 from utils import get_moving_avg, today_str, get_month, remove_waw, get_weekday, dict_counter
@@ -25,20 +23,25 @@ def process_df(df: pd.DataFrame = None) -> pd.DataFrame:
 def get_flats_stats(conn=None) -> dict:
     """ Create statistics about the scraped data """
 
+    print("Loading df_flats ... ")
     df_flats = load_df(conn)
+    print("Processing df_flats ... ")
     df_flats = process_df(df_flats)
     today = today_str()
     df_flats = df_flats.loc[df_flats['date_scraped'] != today]
 
     posted_per_day = dict_counter(df_flats, 'date_posted')
     scraped_per_day = dict_counter(df_flats, 'date_scraped')
+    print("Calculating scraped_per_day ... ")
     scraped_per_day_m_avg = get_moving_avg(scraped_per_day, 7)
     scraped_per_month = dict_counter(df_flats, 'month')
 
     flats_per_location = dict_counter(df_flats, 'location')
     flats_per_area_cat = dict_counter(df_flats, 'area_category')
 
+    print("Loading load_df_avg_prices ... ")
     price_m_location = load_df_avg_prices(conn)
+    print("Loading load_area_cat_df ... ")
     price_m_loc_area_cat = load_area_cat_df(conn)
 
     price_m_location = process_df(price_m_location).to_dict('record')
--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,4 @@
 from collections import Counter
-
 import pandas as pd
 from datetime import datetime
 
