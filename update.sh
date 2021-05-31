dec58338279511abf37ad318ad22af597bf3fde8
mbalcerzak <malgorzata.a.balcerzak@gmail.com>
2021-06-01 00:39:17 +0200

    updated json

--- update.sh
+++ update.sh
@@ -2,8 +2,10 @@ cd /home/pi/Documents/warsaw_flats_api/
 
 git pull
 
+git checkout raspberry-updates
+
 source venv/bin/activate
-python3 /home/pi/Documents/warsaw_flats_api/api/convert_to_json.py
+
 
 git add .
 
